1631b24d7b0611c1b4b1214e7830184548077efa
Graeme <firewarrior2112@gmail.com>
2015-12-01 15:07:05 -0600

    first commit
    a couple of classes for the item and inventory systems

---
+++ DungeonCrawlerV0.0/Assets/Item.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    string name = "";
+    float weight = 0.0f;
+    int value = 0;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
